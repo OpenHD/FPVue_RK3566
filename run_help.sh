910612d82b9a00ce1f020edc954f4a69cc3765cc
Consti10 <geierconstantinabc@gmail.com>
2024-04-02 17:41:01 +0200

    exp memset

--- run_help.sh
+++ run_help.sh
@@ -12,7 +12,7 @@
 # H264 udp
 gst-launch-1.0 udpsrc port=5600 caps='application/x-rtp, payload=(int)96, clock-rate=(int)90000, media=(string)video, encoding-name=(string)H264' ! rtph264depay ! h264parse config-interval=1 !  \
 video/x-h264,stream-format=byte-stream,alignment=au ! \
-fdsink fd=1 | ./build/fpvue  --rmode 6
+fdsink fd=1 | ./build/fpvue  --rmode 10
 
 # H265 udp
 #gst-launch-1.0  udpsrc port=5600 caps = "application/x-rtp, media=(string)video, encoding-name=(string)H265" ! rtph265depay !  h265parse config-interval=-1 ! \
