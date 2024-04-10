2e9b80ba8c6b0bf8c649ffaf2f467168e7011b29
Consti10 <geierconstantinabc@gmail.com>
2024-04-11 01:21:23 +0200

    another neon memcpy

--- copy_util.h
+++ copy_util.h
@@ -20,7 +20,7 @@ void memcpy_neon_8(uint8_t* region2, const uint8_t* region1, size_t length){
 
     for (const uint8_t *end = region1 + length; region1 < end; region1 += 8, region2 += 8) {
         in = vld1_u8(region1);
-        vst1_u8(region1, out);
+        vst1_u8(region2, in);
     }
 }
 
