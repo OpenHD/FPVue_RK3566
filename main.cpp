15c583948d3509fe1db8779e39bc4dce38873b7d
Consti10 <geierconstantinabc@gmail.com>
2024-04-10 23:04:30 +0200

    exp copy to buffer

--- main.cpp
+++ main.cpp
@@ -150,7 +150,7 @@ void map_copy_unmap(int fd_src,int fd_dst,int memory_size){
 
     //memcpy(dst_p,src_p,memory_size);
     uint64_t before_memcpy=get_time_ms();
-    memcpy_threaded(test_buffer,src_p,memory_size,3);
+    memcpy_threaded(dst_p,&test_buffer,memory_size,3);
     end_sync(fd_src,false);
     end_sync(fd_dst,true);
     //uint64_t elapsed_memcpy=get_time_ms()-before_memcpy;
