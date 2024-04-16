3abf3908167785c9f2d5bc0da9766263b54c3ce2
Consti10 <geierconstantinabc@gmail.com>
2024-04-16 18:26:11 +0200

    ... and another memcpy

--- copy_util.h
+++ copy_util.h
@@ -48,17 +48,22 @@ void memcpy_neon_aligned(void* dst, const void * src, size_t length){
 }*/
 
 // From https://stackoverflow.com/questions/34888683/arm-neon-memcpy-optimized-for-uncached-memory
+// and https://stackoverflow.com/questions/61210517/memcpy-for-arm-uncached-memory-for-arm64
 void my_copy(unsigned char *dst,unsigned char *src, int sz){
     if (sz & 63) {
         sz = (sz & -64) + 64;
     }
-    asm volatile (
-            "NEONCopyPLD:                          \n"
-            "    VLDM %[src]!,{d0-d7}                 \n"
-            "    VSTM %[dst]!,{d0-d7}                 \n"
-            "    SUBS %[sz],%[sz],#0x40                 \n"
-            "    BGT NEONCopyPLD                  \n"
-            : [dst]"+r"(dst), [src]"+r"(src), [sz]"+r"(sz) : : "d0", "d1", "d2", "d3", "d4", "d5", "d6", "d7", "cc", "memory");
+    asm volatile ("NEONCopyPLD: \n"
+                  "sub %[dst], %[dst], #64 \n"
+                  "1: \n"
+                  "ldnp q0, q1, [%[src]] \n"
+                  "ldnp q2, q3, [%[src], #32] \n"
+                  "add %[dst], %[dst], #64 \n"
+                  "subs %[sz], %[sz], #64 \n"
+                  "add %[src], %[src], #64 \n"
+                  "stnp q0, q1, [%[dst]] \n"
+                  "stnp q2, q3, [%[dst], #32] \n"
+                  "b.gt 1b \n");
 }
 
 
