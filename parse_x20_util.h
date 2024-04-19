f49f8522c894f7f1e517f9b17aa87d963319ad5a
Consti10 <geierconstantinabc@gmail.com>
2024-04-19 02:24:24 +0200

    try and fix x20

--- parse_x20_util.h
+++ parse_x20_util.h
@@ -19,7 +19,7 @@ static int check_for_x20(const uint8_t* data, int data_len){
     if(data_len<3)return -1;
     NALU tmp(data,data_len);
     const auto type=tmp.get_nal_unit_type();
-    printf("Type:%s",tmp.get_nal_unit_type_as_string().c_str());
+    printf("Type:%s\n",tmp.get_nal_unit_type_as_string().c_str());
     if(type==NALUnitType::H264::NAL_UNIT_TYPE_SPS){
         printf("Got SPS\n");
     }
