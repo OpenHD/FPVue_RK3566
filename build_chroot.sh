9ed826ecd2f4a3bc26f1dbbcd02c747c0245e3e9
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2024-08-03 23:04:42 +0200

    disable upload

--- build_chroot.sh
+++ build_chroot.sh
@@ -33,15 +33,3 @@ API_KEY=$(cat /opt/additionalFiles/cloudsmith_api_key.txt)
 DISTRO=$(cat /opt/additionalFiles/distro.txt)
 FLAVOR=$(cat /opt/additionalFiles/flavor.txt)
 BOARD=$(cat /opt/additionalFiles/board.txt)
-
-if [ "$BOARD" = "rk3588" ]; then
-    for file in *.deb; do
-        mv "$file" "${file%.deb}-rk3588.deb"
-    done
-    cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
-else
-for file in *.deb; do
-        mv "$file" "${file%.deb}-rk3566.deb"
-    done
-    cloudsmith push deb --api-key "$API_KEY" openhd/dev-release/${DISTRO}/${FLAVOR} *.deb || exit 1
-fi
