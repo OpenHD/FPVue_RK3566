643283f700cab3d767e7d09c2adb799bfb746522
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-09-07 05:03:37 +0200

    Use DRM fd from display host for color cycle

--- main.cpp
+++ main.cpp
@@ -1029,10 +1029,19 @@ bool color_cycle=false;
 int run_color_cycle(uint16_t mode_width, uint16_t mode_height, uint32_t mode_vrefresh){
     int ret;
     int fd;
-    ret = modeset_open(&fd, "/dev/dri/card0");
-    if (ret < 0) {
-        printf("modeset_open() =  %d\n", ret);
-        return 1;
+    const char* fd_socket = getenv("FPVUE_DRM_FD_SOCKET");
+    if (fd_socket) {
+        fd = receive_fd_from_socket(fd_socket);
+        if (fd < 0) {
+            fprintf(stderr, "Failed to receive DRM FD from socket %s\n", fd_socket);
+            return 1;
+        }
+    } else {
+        ret = modeset_open(&fd, "/dev/dri/card0");
+        if (ret < 0) {
+            printf("modeset_open() =  %d\n", ret);
+            return 1;
+        }
     }
     struct modeset_output *out = (struct modeset_output *)malloc(sizeof(struct modeset_output));
     ret = modeset_prepare(fd, out, mode_width, mode_height, mode_vrefresh);
