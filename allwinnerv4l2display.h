2fe44f4d79235eccb58150f76fc6519c09349c5a
Raphael <68374617+raphaelscholle@users.noreply.github.com>
2025-10-27 20:27:41 +0100

    Switch Allwinner path to Cedar OpenMAX decoder

--- allwinnerv4l2display.h
+++ allwinnerv4l2display.h
@@ -2,6 +2,9 @@
 #define FPVUE_ALLWINNERV4L2DISPLAY_H
 
 #include <atomic>
+#include <condition_variable>
+#include <mutex>
+#include <queue>
 #include <string>
 #include <thread>
 #include <vector>
@@ -11,11 +14,22 @@ extern "C" {
 #include "drm.h"
 }
 
+#ifndef HAVE_AW_OMX
+#define HAVE_AW_OMX 0
+#endif
+
+#if HAVE_AW_OMX
+extern "C" {
+#include <OMX_Component.h>
+#include <OMX_Core.h>
+}
+#endif
+
 /**
  * Experimental display path for Allwinner A733 devices.
  * This class receives RTP frames on a UDP port, decodes them using
- * the V4L2 stateless decoder and presents the frames on a KMS plane
- * driven by the sunxi-drm driver.
+ * the Allwinner Cedar OpenMAX IL decoder and presents the frames on a KMS
+ * plane driven by the sunxi-drm driver.
  */
 class AllwinnerV4L2Display {
 public:
@@ -40,9 +54,22 @@ public:
 private:
   bool setup_network();
   bool setup_drm();
-  bool setup_v4l2();
+  bool setup_omx();
   void decode_loop();
-  int open_candidate_v4l2_device(const char* path);
+  void teardown_omx();
+  void teardown_drm_resources();
+
+#if HAVE_AW_OMX
+  bool wait_for_state(OMX_STATETYPE state, int timeout_ms = 2000);
+  bool ensure_display_buffers(uint32_t width, uint32_t height);
+  void recycle_display_buffers();
+  static OMX_ERRORTYPE omx_event_handler(OMX_HANDLETYPE, OMX_PTR, OMX_EVENTTYPE,
+                                         OMX_U32, OMX_U32, OMX_PTR);
+  static OMX_ERRORTYPE omx_empty_buffer_done(OMX_HANDLETYPE, OMX_PTR,
+                                             OMX_BUFFERHEADERTYPE*);
+  static OMX_ERRORTYPE omx_fill_buffer_done(OMX_HANDLETYPE, OMX_PTR,
+                                            OMX_BUFFERHEADERTYPE*);
+#endif
 
   int m_port;
   bool m_h265;
@@ -52,7 +79,6 @@ private:
 
   int m_sock{-1};
   int m_input_fd{-1};
-  int m_v4l2_fd{-1};
   int m_drm_fd{-1};
   bool m_take_ownership_of_drm_fd{true};
   InputMode m_input_mode{InputMode::UDP};
@@ -68,12 +94,36 @@ private:
 
   struct modeset_output m_output{};
 
-  std::string m_v4l2_device_path;
-  std::string m_v4l2_driver_name;
+#if HAVE_AW_OMX
+  OMX_HANDLETYPE m_decoder{nullptr};
+  OMX_CALLBACKTYPE m_callbacks{};
+  OMX_STATETYPE m_decoder_state{OMX_StateLoaded};
+  std::vector<OMX_BUFFERHEADERTYPE*> m_input_buffers;
+  std::vector<OMX_BUFFERHEADERTYPE*> m_output_buffers;
+  std::queue<OMX_BUFFERHEADERTYPE*> m_available_inputs;
+  std::queue<OMX_BUFFERHEADERTYPE*> m_filled_outputs;
+  std::mutex m_input_mutex;
+  std::condition_variable m_input_cv;
+  std::mutex m_output_mutex;
+  std::condition_variable m_output_cv;
+  std::mutex m_state_mutex;
+  std::condition_variable m_state_cv;
+  bool m_port_settings_dirty{false};
 
-  std::vector<void*> m_output_buffers;
-  std::vector<void*> m_capture_buffers;
-  std::vector<int> m_capture_fbs;
+  struct DrmBuffer {
+    uint32_t fb_id{0};
+    uint32_t handle{0};
+    void* map{nullptr};
+    size_t size{0};
+    uint32_t pitches[2]{};
+    uint32_t offsets[2]{};
+  };
+  std::vector<DrmBuffer> m_drm_buffers;
+  uint32_t m_display_stride{0};
+  uint32_t m_allocated_width{0};
+  uint32_t m_allocated_height{0};
+  uint32_t m_drm_buffer_index{0};
+#endif
 };
 
 #endif // FPVUE_ALLWINNERV4L2DISPLAY_H
